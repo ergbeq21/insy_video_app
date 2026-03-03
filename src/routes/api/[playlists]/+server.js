21e9fc0aa8d201077eb20d6da0c285bda0bc1fd3
Eriseldi <eribeq21@htl-shkoder.com>
2026-03-03 10:41:18 +0100

    feat: develop a new api endpoint to get a single playlist by id

---
+++ src/routes/api/[playlists]/+server.js
@@ -0,0 +1,22 @@
+import dbPromise from '$lib/server/mongo';
+import { ObjectId } from 'mongodb';
+
+export const GET = async ({ params }) => {
+    const { playlists: id } = params;
+
+    const db = await dbPromise;
+    const playlists = db.collection("playlists");
+    const playlist = await playlists.findOne({ _id: new ObjectId(id) });
+
+    if (!playlist) {
+        return new Response(JSON.stringify({ error: 'Playlist not found' }), { status: 404 });
+    }
+
+    const serializedPlaylist = {
+        ...playlist,
+        _id: playlist._id.toString()
+    };
+
+    return new Response(JSON.stringify(serializedPlaylist), { status: 200 });
+    
+}
