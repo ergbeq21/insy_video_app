48dac4a5877340d3483e8f4d846305fd765bf627
Eriseldi <eribeq21@htl-shkoder.com>
2026-03-03 10:16:07 +0100

    Feat: Display Playlists in  the homepage

---
+++ src/routes/home/+page.server.js
@@ -0,0 +1,18 @@
+import dbPromise from '$lib/server/mongo';
+
+export async function load(){
+    const db = await dbPromise;
+    const playlists  = db.collection("playlists");
+    const playlistsRows = await playlists.find({}).toArray();
+
+    const serializedPlaylists = playlistsRows.map(playlist => ({
+        ...playlist,
+        _id: playlist._id.toString()
+    }));
+
+    return {
+        playlists: serializedPlaylists
+    };
+
+}
+
---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    let { data } = $props();
+
+    let playlists = $derived(data.playlists);
+
+
+</script>
+
+<h1>Playlists</h1>
+<ul>
+    {#each playlists as playlist}
+        <li>
+            <a href={`/playlist/${playlist.id}`} class="text-blue-500 hover:underline">
+                {playlist.name} 
+            </a>
+        </li>
+    {/each}
+</ul>
